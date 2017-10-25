d65d11b37d20b566c96cd6a35a2823e9db381244
THWest <thwest001@hotmail.com>
2017-10-25 22:20:38 +0700

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,37 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { UserComponent } from './components/user/user.component';
+import { AboutusComponent } from './components/aboutus/aboutus.component';
+import { FormsModule } from '@angular/forms'
+import { HttpModule } from '@angular/http'
+import { RouterModule, Routes} from "@angular/router";
+
+import { GetphotoService } from './services/getphoto.service.';
+import { ProfileComponent } from './components/profile/profile.component';
+
+const appRoutes:Routes = [
+  {path:"",component:UserComponent},
+  {path:"aboutus",component:AboutusComponent},
+  {path:"profile",component:ProfileComponent}
+]
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    UserComponent,
+    AboutusComponent,
+    ProfileComponent
+    
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    RouterModule.forRoot(appRoutes)
+  ],
+  providers: [ GetphotoService ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
